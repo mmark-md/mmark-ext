cdcc06af9ee178f88fbef7f24a202b8d852df851
mrkkrp <markkarpov92@gmail.com>
2018-01-09 22:21:28 +0700

    Make punctuation prettifier handle more punctuation

--- Text/MMark/Extension/PunctuationPrettifier.hs
+++ Text/MMark/Extension/PunctuationPrettifier.hs
@@ -16,20 +16,56 @@ module Text.MMark.Extension.PunctuationPrettifier
   ( punctuationPrettifier )
 where
 
+import Data.Char (isSpace)
+import Data.Text (Text)
 import Text.MMark.Extension (Extension, Inline (..))
 import qualified Data.Text as T
 import qualified Text.MMark.Extension as Ext
 
 -- | Prettify punctuation (only affects plain text in inlines):
---     * Replace @---@ with em-dash
---     * Replace @--@ with en-dash
---     * Replace @...@ with ellipsis
---     * Replace @'@ with apostrophe where makes sense
+--
+--     * Replace @...@ with ellipsis @…@
+--     * Replace @---@ with em-dash @—@
+--     * Replace @--@ with en-dash @–@
+--     * Replace @\"@ with left double quote @“@ when previous character was
+--       a space character, otherwise replace it with right double quote @”@
+--     * Replace @'@ with left single quote @‘@ when previous character was
+--       a space character, otherwise replace it with right single quote @’@
+--       aka apostrophe
 
 punctuationPrettifier :: Extension
 punctuationPrettifier = Ext.inlineTrans $ \case
-  Plain txt -> Plain
-    . T.replace "--"  "–"
-    . T.replace "---" "—"
-    $ txt
-  other -> other
+  Plain txt -> Plain (T.unfoldr gen (True, [], txt))
+  other     -> other
+
+gen
+  :: (Bool, String, Text)
+     -- ^ Whether the previous character was a space, characters pending
+     -- insertion, and remaining input
+  -> Maybe (Char, (Bool, String, Text))
+     -- ^ Next generated char and the state
+gen (s, x:xs, i) = Just (x, (s, xs, i))
+gen (s, [],   i) =
+  case T.uncons i of
+    Nothing -> Nothing
+    Just ('.', i') ->
+      case T.splitAt 2 i' of
+        ("..", i'') -> Just ('…', (False, [], i''))
+        (xs,   i'') -> Just ('.', (False, T.unpack xs, i''))
+    Just ('-', i') ->
+      case T.splitAt 2 i' of
+        ("--", i'') -> Just ('—', (False, [], i''))
+        _ ->
+          case T.splitAt 1 i' of
+            ("-", i'') -> Just ('–', (False, [], i''))
+            (xs,  i'') -> Just ('-', (False, T.unpack xs, i''))
+    Just ('\"', i') ->
+      if s -- whether previous character was a space character
+        then Just ('“', (False, [], i'))
+        else Just ('”', (False, [], i'))
+    Just ('\'', i') ->
+      if s
+        then Just ('‘', (False, [], i'))
+        else Just ('’', (False, [], i'))
+    Just (ch, i') ->
+      Just (ch, (isSpace ch, [], i'))
--- tests/Text/MMark/Extension/PunctuationPrettifierSpec.hs
+++ tests/Text/MMark/Extension/PunctuationPrettifierSpec.hs
@@ -11,10 +11,20 @@ spec =
   describe "punctuationPrettifier" $ do
     let to = withExt punctuationPrettifier
     context "on plain inlines" $ do
-        it "replaces -- with en dash" $
-          "Here we go -- at last." `to` "<p>Here we go – at last.</p>\n"
+        it "replaces ... with ellipsis" $
+          "He forgot where he came from..." `to` "<p>He forgot where he came from…</p>\n"
         it "replaces --- with em dash" $
           "Here we go---at last." `to` "<p>Here we go—at last.</p>\n"
+        it "replaces -- with en dash" $
+          "Here we go -- at last." `to` "<p>Here we go – at last.</p>\n"
+        it "replaces double quotes intelligently" $ do
+          "\"Something\"" `to` "<p>“Something”</p>\n"
+          "\"\"" `to` "<p>“”</p>\n"
+        it "replaces single quotes intelligently" $ do
+          "I'm doin' well, 'cause I care 'bout 'Big Jim'."
+            `to` "<p>I’m doin’ well, ‘cause I care ‘bout ‘Big Jim’.</p>\n"
+          "'Something'" `to` "<p>‘Something’</p>\n"
+          "''" `to` "<p>‘’</p>\n"
     context "on other inlines" $
       it "has no effect" $
         "`code -- span`" `to` "<p><code>code -- span</code></p>\n"
